2b867429d284eeb802f83547afb59b914cb46e95
engineer135 <everafterk@naver.com>
2018-04-12 21:31:56 +0900

    약수 중 최대값 구하기...좌절...

--- p3_largest_prime_factor.c
+++ p3_largest_prime_factor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 typedef enum _boolean {
 	FALSE,
 	TRUE
@@ -14,40 +15,70 @@ boolean checkPrimeFactor(long long num) {
 	}
 	return TRUE;
 }
+*/
 
 void main(void) {
 	/*
 	The prime factors of 13195 are 5, 7, 13 and 29.
 	What is the largest prime factor of the number 600851475143 ?
 	*/
-	
+
 	printf("start\n");
 
+	/*
 	boolean isPrimeFactor = FALSE;
 	long long num = 313;
-	
+
 	// 2부터 (자기자신/2)까지만 체크하면 된다. 약수는 (자기자신/2)보다 클 수 없기 때문이다.
 	// 제일 큰 약수만 찾으면 된다.
 	for (long long i = num/2; i > 2; i--) {
-		printf("%lld\n", i);
-		//printf("num % i === %lld\n", (num % i));
-		if (num % i == 0) {
-			printf("약수 %lld\n", i);
-			// 소수인지 체크
-			if(checkPrimeFactor(i)) {
-				printf("소수 맞음 %lld\n", i);
-				num = i; // 소수 담는다.
-				break;
-			}
-		}
-		else {
-			//printf("약수 아님 %lld\n", i);
+	printf("%lld\n", i);
+	//printf("num % i === %lld\n", (num % i));
+	if (num % i == 0) {
+	printf("약수 %lld\n", i);
+	// 소수인지 체크
+	if(checkPrimeFactor(i)) {
+	printf("소수 맞음 %lld\n", i);
+	num = i; // 소수 담는다.
+	break;
+	}
+	}
+	else {
+	//printf("약수 아님 %lld\n", i);
+	}
+	}
+
+	printf("largest prime factor === %lld\n", num);
+	*/
+
+	/*
+	아래 성질을 이용해서 알고리즘 다시 짠다...
+	10 의 약수
+	2 5 10
+
+	10%2 = 0
+	5%2 = 1
+	5%3 = 2
+	5%4 = 1
+	5%5 = 0
+
+	1 끝
+	*/
+
+	long long num = 600851475143;
+	long long largestPrimeFactor = 0;
+	for (long long i = 2; i <= num; i++) {
+		if (num%i == 0) {
+			//printf("%d\n", i);
+			largestPrimeFactor = i;
+			num = num / i;
+			i = i - 1;
 		}
 	}
-	
-	printf("largest prime factor === %lld\n", num);
 
-	
+	printf("largestPrimeFactor : %lld\n", largestPrimeFactor); // 6857 헐 대박...........
+	// 이게 difficulty가 5%면 앞으로 어떤 문제가 나온다는건지 ㅠㅠ
+
 	printf("end\n");
 
 }
