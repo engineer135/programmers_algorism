db4dce146ec637155270e9d4833657886b6bf35e
kanghk <everafterk@naver.com>
2018-05-25 09:38:27 +0900

    1-1000까지 숫자 letter의 수 구하기. fourty가 아닌 forty가 맞다. 이런!

--- p17_Number_letter_counts.py
+++ p17_Number_letter_counts.py
@@ -15,35 +15,38 @@ one thousand
 '''
 
 digit = ('','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen')
-tenDigit = ('','','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety')
+tenDigit = ('','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety')
 
 letterCnt = 0
 
 for i in range(1,1001):
     if i < 20:
-        print(digit[i])
+        #print(digit[i])
         #print(len(digit[i]))
         letterCnt += len(digit[i])
     elif i < 100:
-        print(tenDigit[int(i/10)],'',digit[int(i%10)])
+        #print(tenDigit[int(i/10)],'',digit[int(i%10)])
         #print(len(tenDigit[int(i/10)]) + len(digit[int(i%10)]))
         letterCnt += len(tenDigit[int(i/10)]) + len(digit[int(i%10)])
     elif i >= 1000:
-        print('one thousand') # 11자리 더해준다.
+        #print('one thousand') # 11자리 더해준다.
         letterCnt += 11
     else:
         if i%100 < 20:
-            print(digit[int(i/100)],'hundred and',digit[int(i%100)])
+            #print(digit[int(i/100)],'hundred and',digit[int(i%100)])
             #print(len(digit[int(i/100)]) + len(digit[int(i%100)]) + 10) #'hundred and' 글자 개수 10개 더 더해줘야함.
             letterCnt += len(digit[int(i/100)]) + len(digit[int(i%100)]) + 10
         else:
-            print(digit[int(i/100)],'hundred and',tenDigit[int(str(i)[1:2])],'',digit[int(str(i)[2:])])
+            #print(digit[int(i/100)],'hundred and',tenDigit[int(str(i)[1:2])],'',digit[int(str(i)[2:])])
             #print(len(digit[int(i/100)]) + len(tenDigit[int(str(i)[1:2])]) + len(digit[int(str(i)[2:])]) + 10) #'hundred and' 글자 개수 10개 더 더해줘야함.
             letterCnt += len(digit[int(i/100)]) + len(tenDigit[int(str(i)[1:2])]) + len(digit[int(str(i)[2:])]) + 10
 
         if i%100 == 0:
+            #print(i)
             letterCnt -= 3
             #100으로 나누어 떨어지면 and가 필요 없으므로 -3
 
 print(letterCnt) #21224
 # 오잉 왜 답이 틀리다고 나오지...?
+# OMG... 40이 fourty가 아니고 forty구나...
+# 21124 가 답...
